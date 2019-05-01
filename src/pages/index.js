c801ace99df31919149cb5352c6a173d3bdf66c0
eneax <19818937+eneax@users.noreply.github.com>
2019-05-01 13:29:05 +0200

    Add animations to buy btn

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ const IndexPage = () => (
 
     <div className="hero">
       <div className="heroGroup">
-        <h1>Learn to design and code React apps</h1>
+        <h1>Learn to <br/> design and code React apps</h1>
         <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
 
         <Link to="/page-2/">Watch the video</Link>
