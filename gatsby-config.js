d43a73522e12ec39b1e2e81a4bd0bbde12bee046
eneax <19818937+eneax@users.noreply.github.com>
2019-05-01 11:23:20 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Design Code`,
+    description: `Design Code website built with Gatsby`,
+    author: `@eneax`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -29,6 +29,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,9 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Learn to design and code React apps</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+    
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
