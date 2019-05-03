6aa78f577d2ff5f73d3c61c8a0f0f5592455f2b5
eneax <19818937+eneax@users.noreply.github.com>
2019-05-03 21:25:13 +0200

    Add top and bottom waves to section

--- src/components/section.js
+++ src/components/section.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import Wave from '../components/wave'
 
 const SectionGroup = styled.div`
   background: url(${props => props.image});
@@ -8,6 +9,11 @@ const SectionGroup = styled.div`
   display: grid;
   grid-template-rows: 300px auto;
   grid-gap: 20px;
+  position: relative;
+  
+  @media (max-width: 640px) {
+    height: 820px;
+  }
 `
 
 const SectionLogo = styled.img`
@@ -22,7 +28,7 @@ const SectionTitleGroup = styled.div`
   margin: 0 40px;
   grid-gap: 20px;
   grid-template-rows: auto 100%;
-
+  
   @media (max-width: 720px) {
     grid-template-columns: 1fr;
   }
@@ -33,21 +39,41 @@ const SectionTitle = styled.h3`
   font-size: 60px;
   margin: 0;
   line-height: 1.2;
+  
+  @media (max-width: 720px) {
+    font-size: 40px;
+  }
 `
 
 const SectionText = styled.p`
   color: white;
 `
 
+const WaveBottom = styled.div`
+  position: absolute;
+  width: 100%;
+  bottom: -6px;
+`
 
-const Section = (props) => (
-  <SectionGroup image={props.image}>
-    <SectionLogo src={props.logo} />
-    <SectionTitleGroup>
-      <SectionTitle>{props.title}</SectionTitle>
-      <SectionText>{props.text}</SectionText>
-    </SectionTitleGroup>
-  </SectionGroup>
+const WaveTop = styled.div`
+  position: absolute;
+  width: 100%;
+  top: -6px;
+  transform: rotate(180deg);
+`
+
+const Section = props => (
+	<SectionGroup image={props.image}>
+    <WaveTop><Wave /></WaveTop>
+    <WaveBottom><Wave /></WaveBottom>
+
+		<SectionLogo src={props.logo} />
+
+		<SectionTitleGroup>
+			<SectionTitle>{props.title}</SectionTitle>
+			<SectionText>{props.text}</SectionText>
+		</SectionTitleGroup>
+	</SectionGroup>
 )
 
 export default Section
