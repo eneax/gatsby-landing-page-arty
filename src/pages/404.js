7e1a6896eb130fc0fd4825e80f559e3c5594cde3
eneax <19818937+eneax@users.noreply.github.com>
2019-06-02 22:39:57 +0200

    Add basic styles to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,33 @@
 import React from "react"
+import styled from 'styled-components'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+
+const HeroGroup = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  padding: 150px 50px;
+  text-align: center;
+
+  @media (max-width: 640px) {
+    padding: 100px 20px;
+  }
+`
+
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <div className="hero">
+
+    <HeroGroup>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </HeroGroup>
+    </div>
   </Layout>
 )
 
