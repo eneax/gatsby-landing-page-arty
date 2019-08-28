d98cdff8a687f8d1f9268cd801d8fc38bd62a8fe
eneax <19818937+eneax@users.noreply.github.com>
2019-08-28 22:22:59 +0200

    Create video component

---
+++ src/components/video.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Video = ({ videoSrcURL, videoTitle, ...props }) => (
+  <div className="video">
+    <iframe
+      src={videoSrcURL}
+      title={videoTitle}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      frameBorder="0"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true"
+      allowFullScreen
+    />
+  </div>
+)
+
+export default Video
+
