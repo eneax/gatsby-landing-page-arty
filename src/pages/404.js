e1c070a6e2b25eae5373e9e1bbd79e73948453c6
eneax <19818937+eneax@users.noreply.github.com>
2019-09-17 21:18:40 +0200

    Style NotFoundPage

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,30 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 
-const HeroGroup = styled.div`
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+
+    <ContentWrapper>
+      <ContentGroup>
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </ContentGroup>
+    </ContentWrapper>
+
+  </Layout>
+)
+
+export default NotFoundPage
+
+
+// styles
+const ContentWrapper = styled.div`
+  background: rgba(0,0,0,.7);
+  color: #fff;
+`
+
+const ContentGroup = styled.div`
   max-width: 500px;
   margin: 0 auto;
   padding: 250px 50px;
@@ -15,20 +38,3 @@ const HeroGroup = styled.div`
     padding: 150px 20px;
   }
 `
-
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-
-    <div className="hero">
-
-    <HeroGroup>
-      <h1>NOT FOUND</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-      </HeroGroup>
-    </div>
-  </Layout>
-)
-
-export default NotFoundPage
