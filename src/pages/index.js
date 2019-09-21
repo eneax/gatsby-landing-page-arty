b4d97c395394d6c5ee8097e4fa820bfd386882da
eneax <19818937+eneax@users.noreply.github.com>
2019-09-21 18:14:48 +0200

    Update SEO meta for home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React from "react"
+import React from 'react'
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-import Hero from "../components/hero";
-import Cards from "../components/cards"
-import Section from "../components/section"
-import Cells from "../components/cells";
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Hero from '../components/hero';
+import Cards from '../components/cards'
+import Section from '../components/section'
+import Cells from '../components/cells';
 
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title='Arty' keywords={[`gatsby`, `landing page`, `react`, `contentful`, `graphql`]} />
 
     <Hero />
     <Cards />
