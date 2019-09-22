beff286f0a803a83514fed9d54b680bd387c277b
eneax <19818937+eneax@users.noreply.github.com>
2019-09-22 11:05:23 +0200

    Fix manifest display warning

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,7 +27,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `Arty`,
+        display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
